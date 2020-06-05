2df7a6c12676c3098cb922612ebefdc9e6eaac3f
Jens Reidel <jens@troet.org>
2020-06-05 12:13:38 +0200

    Remove test script

--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-req = requests.post("http://localhost:3000/api/imageops/oil", json={"image": "https://cdn.discordapp.com/avatars/356091260429402122/cb01d86552cc783532cabdb7ad1c70bd.png?size=1024"})
-open("test.png", "wb").write(req.content)
