6eac1f47d97371debcf5c8b768d01852d7dd6e0e
Jens Reidel <adrian@travitia.xyz>
2020-11-16 16:10:39 +0100

    Update resvg and use tiny-skia

--- src/routes/chess.rs
+++ src/routes/chess.rs
@@ -1,11 +1,9 @@
 use crate::encoder::encode_png;
 use actix_web::{post, web::Json, HttpResponse};
-use image::{Bgra, DynamicImage, ImageBuffer};
-use resvg_raqote::{render_to_image, Options};
+use image::RgbaImage;
+use resvg::render;
 use serde::Deserialize;
 use usvg::{FitTo, Tree};
-
-type BgraImage = ImageBuffer<Bgra<u8>, Vec<u8>>;
 
 #[derive(Deserialize)]
 struct ChessJson {
@@ -26,17 +24,10 @@ async fn genchess(body: Json<ChessJson>) -> HttpResponse {
             ))
         }
     };
-    let mut options = Options::default();
-    options.fit_to = FitTo::Width(390);
-    let img = render_to_image(&tree, &options).expect("rendering yielded no result");
-    let width = img.width() as u32;
-    let height = img.height() as u32;
-    let vect = img.get_data_u8();
-    let dyn_image = DynamicImage::ImageBgra8(
-        BgraImage::from_vec(width, height, vect.to_vec())
-            .expect("RGBA data does not match height and width"),
-    );
-    let final_image = encode_png(&dyn_image.to_rgba()).expect("encoding PNG failed");
+    let img = render(&tree, FitTo::Width(390), None).expect("rendering yielded no result");
+    let vect = img.take();
+    let image = RgbaImage::from_raw(390, 390, vect).unwrap();
+    let final_image = encode_png(&image).expect("encoding PNG failed");
     HttpResponse::Ok()
         .content_type("image/png")
         .body(final_image)
