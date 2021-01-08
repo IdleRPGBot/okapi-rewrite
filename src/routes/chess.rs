db9ab34d0535e7defd7ecc58340e8f2cff90a280
Jens Reidel <adrian@travitia.xyz>
2021-01-08 16:10:57 +0100

    Update to tokio 1.0

--- src/routes/chess.rs
+++ src/routes/chess.rs
@@ -3,6 +3,7 @@ use actix_web::{post, web::Json, HttpResponse};
 use image::RgbaImage;
 use resvg::render;
 use serde::Deserialize;
+use tiny_skia::Pixmap;
 use usvg::{FitTo, Tree};
 
 #[derive(Deserialize)]
@@ -24,8 +25,9 @@ async fn genchess(body: Json<ChessJson>) -> HttpResponse {
             ))
         }
     };
-    let img = render(&tree, FitTo::Width(390), None).expect("rendering yielded no result");
-    let vect = img.take();
+    let mut map = Pixmap::new(390, 390).unwrap();
+    render(&tree, FitTo::Width(390), map.as_mut()).expect("rendering yielded no result");
+    let vect = map.take();
     let image = RgbaImage::from_raw(390, 390, vect).unwrap();
     let final_image = encode_png(&image).expect("encoding PNG failed");
     HttpResponse::Ok()
