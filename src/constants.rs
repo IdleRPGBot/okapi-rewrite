1dc672f775a8748068da590b764a41b8f8ab27f5
Jens Reidel <adrian@travitia.xyz>
2021-11-04 19:05:29 +0100

    use webp internally too

--- src/constants.rs
+++ src/constants.rs
@@ -16,62 +16,62 @@ lazy_static! {
         FontVec::try_from_vec(include_bytes!("../assets/fonts/MergedNoKern.otf").to_vec())
             .expect("could not load font");
     pub static ref PROFILE: RgbaImage =
-        load_from_memory(include_bytes!("../assets/images/ProfileOverlayNew.png"))
+        load_from_memory(include_bytes!("../assets/images/ProfileOverlayNew.webp"))
             .expect("Could not load image")
             .to_rgba8();
     pub static ref DEFAULT_PROFILE: RgbaImage =
-        load_from_memory(include_bytes!("../assets/images/ProfileNew.png"))
+        load_from_memory(include_bytes!("../assets/images/ProfileNew.webp"))
             .expect("Could not load image")
             .to_rgba8();
     pub static ref CASTS: HashMap<String, RgbaImage> = {
         let mut all_casts = HashMap::new();
         all_casts.insert(
             "thief",
-            include_bytes!("../assets/images/casts/thief.png").to_vec(),
+            include_bytes!("../assets/images/casts/thief.webp").to_vec(),
         );
         all_casts.insert(
             "paragon",
-            include_bytes!("../assets/images/casts/paragon.png").to_vec(),
+            include_bytes!("../assets/images/casts/paragon.webp").to_vec(),
         );
         all_casts.insert(
             "ranger",
-            include_bytes!("../assets/images/casts/ranger.png").to_vec(),
+            include_bytes!("../assets/images/casts/ranger.webp").to_vec(),
         );
         all_casts.insert(
             "warrior",
-            include_bytes!("../assets/images/casts/warrior.png").to_vec(),
+            include_bytes!("../assets/images/casts/warrior.webp").to_vec(),
         );
         all_casts.insert(
             "mage",
-            include_bytes!("../assets/images/casts/mage.png").to_vec(),
+            include_bytes!("../assets/images/casts/mage.webp").to_vec(),
         );
         all_casts.insert(
             "raider",
-            include_bytes!("../assets/images/casts/raider.png").to_vec(),
+            include_bytes!("../assets/images/casts/raider.webp").to_vec(),
         );
         all_casts.insert(
             "ritualist",
-            include_bytes!("../assets/images/casts/ritualist.png").to_vec(),
+            include_bytes!("../assets/images/casts/ritualist.webp").to_vec(),
         );
         all_casts.insert(
             "human",
-            include_bytes!("../assets/images/casts/human.png").to_vec(),
+            include_bytes!("../assets/images/casts/human.webp").to_vec(),
         );
         all_casts.insert(
             "elf",
-            include_bytes!("../assets/images/casts/elf.png").to_vec(),
+            include_bytes!("../assets/images/casts/elf.webp").to_vec(),
         );
         all_casts.insert(
             "dwarf",
-            include_bytes!("../assets/images/casts/dwarf.png").to_vec(),
+            include_bytes!("../assets/images/casts/dwarf.webp").to_vec(),
         );
         all_casts.insert(
             "orc",
-            include_bytes!("../assets/images/casts/orc.png").to_vec(),
+            include_bytes!("../assets/images/casts/orc.webp").to_vec(),
         );
         all_casts.insert(
             "jikill",
-            include_bytes!("../assets/images/casts/jikill.png").to_vec(),
+            include_bytes!("../assets/images/casts/jikill.webp").to_vec(),
         );
 
         let mut map: HashMap<String, RgbaImage> = HashMap::new();
@@ -91,94 +91,94 @@ lazy_static! {
         map
     };
     pub static ref ADVENTURES: Vec<RgbImage> = vec![
-        load_from_memory(include_bytes!("../assets/images/adventures/1.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/1.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/2.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/2.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/3.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/3.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/4.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/4.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/5.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/5.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/6.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/6.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/7.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/7.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/8.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/8.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/9.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/9.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/10.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/10.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/11.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/11.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/12.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/12.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/13.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/13.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/14.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/14.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/15.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/15.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/16.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/16.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/17.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/17.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/18.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/18.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/19.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/19.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/20.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/20.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/21.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/21.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/22.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/22.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/23.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/23.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/24.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/24.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/25.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/25.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/26.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/26.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/27.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/27.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/28.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/28.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/29.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/29.webp"))
             .expect("Could not load image")
             .to_rgb8(),
-        load_from_memory(include_bytes!("../assets/images/adventures/30.png"))
+        load_from_memory(include_bytes!("../assets/images/adventures/30.webp"))
             .expect("Could not load image")
             .to_rgb8()
     ];
