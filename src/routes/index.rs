473f8f4f1aa3370ddcbad55212ba3633ad81a1fa
Mary <9-Mary@users.noreply.git.travitia.xyz>
2021-11-11 10:48:08 +0100

    Added legal disclaimer

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -6,5 +6,5 @@ pub fn index() -> Result<Response<Body>> {
     Ok(Response::builder()
         .status(200)
         .header("content-type", "text/plain")
-        .body(Body::from("1"))?)
+        .body(Body::from("Disclaimer: The visual content hosted by this website is user-generated. Each material is purged from our servers within 15 minutes. Therefore we cannot take any responsibility for the uploaded user content. In case of copyright infringement contacting is possible thru support.idlerpg.xyz."))?)
 }
